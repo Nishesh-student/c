1ebf5411c33bc8745d01583505e1f1a669284614
mice-7 <125342143+mice-7@users.noreply.github.com>
2023-05-17 19:11:53 +0530

    Add files via upload

---
+++ print_without_semicolon.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	//using if
+	
+	//if(printf("\n hello world"))
+	
+	//using while
+	/*
+	while(printf("\n Hello World")){
+		break;
+	}*/
+	
+	//while(printf("\n Hello World"))
+	
+	
+	for(printf("\n Hello World"))
+	return 0;
+}
