a751cb07f563d926130aa22abd987196b2100290
mice-7 <125342143+mice-7@users.noreply.github.com>
2023-05-20 09:00:44 +0530

    Update counter_using_dev_C.c

--- counter_using_dev_C.c
+++ counter_using_dev_C.c
@@ -1,12 +1,14 @@
 
 #include<stdio.h>
+#include<windows.h>
 int main(){
 	int i;
 	system("color 04");
 	for(i=5;i>=0;i--){
 		system("color 02");
 		printf("%d",i);
-		sleep(1);
+		Beep(1500,2000);
+		sleep(0.5);
 		system("cls");
 	}
 	printf("\n BOOM");
